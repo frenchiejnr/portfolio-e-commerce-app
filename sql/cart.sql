503856fb8f88aa3149fcc9b45d526f53006d8858
frenchiejnr <frenchiejnr@gmail.com>
2023-12-28 10:34:53 +0000

    fix: remove keys

--- sql/cart.sql
+++ sql/cart.sql
@@ -1,20 +1,20 @@
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('1972-02-05 16:50:42', '1991-04-16 06:13:35');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('2002-10-09 21:31:19', '1988-01-21 07:28:13');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('1995-02-11 21:41:27', '2015-05-03 04:03:02');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('2021-08-15 13:01:10', '1983-05-23 07:51:43');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('2010-12-06 21:50:06', '1988-03-24 01:47:28');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('2000-09-09 12:52:13', '2004-03-03 09:43:19');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('2015-10-09 23:05:18', '1988-09-15 11:55:47');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('1982-05-29 03:31:55', '1997-05-01 02:59:35');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('1986-03-11 05:55:17', '1971-09-14 17:03:01');
-insert into cart (cart_id, created_at, updated_at)
+insert into cart (created_at, updated_at)
 values ('1977-07-14 19:29:37', '1991-10-28 20:00:00');
--- sql/checkout.sql
+++ sql/checkout.sql
@@ -1,68 +1,58 @@
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Credit Card', '09 Petterle Circle', 954.82);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Apple Pay', '23 Bluejay Pass', 963.58);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Credit Card', '825 Sachtjen Way', 703.76);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Debit Card', '87659 Redwing Avenue', 28.49);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Apple Pay', '853 Dwight Point', 787.17);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('PayPal', '74528 Old Gate Pass', 880.08);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('PayPal', '2172 Cottonwood Street', 36.35);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('PayPal', '98 Shelley Lane', 792.07);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
     )
 values ('Debit Card', '8336 Manley Plaza', 811.04);
 insert into checkout (
-        checkout_id,
         payment_method,
         shipping_address,
         total_amount
